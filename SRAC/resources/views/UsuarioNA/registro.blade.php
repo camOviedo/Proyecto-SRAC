f66ba0b75a8efbb1de0121381d5653cfa5cb5ac1
alvarokb2 <pastalvaro@gmail.com>
2015-11-05 18:44:28 -0300

    FIXED-vistas

--- SRAC/resources/views/UsuarioNA/registro.blade.php
+++ SRAC/resources/views/UsuarioNA/registro.blade.php
@@ -1,1 +1,12 @@
-@extends('')
+@extends('layouts.principal')
+
+@section('navegador')
+    <a href="">Home</a>|
+    <a href="">Contacto</a>|
+    <a href="">Ayuda</a>
+    <hr>
+@endsection
+
+@section('contenido')
+    <h2>Registro</h2>
+@endsection
